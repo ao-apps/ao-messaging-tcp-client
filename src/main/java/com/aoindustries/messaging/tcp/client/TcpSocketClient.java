1b1c78d8a6abf1f8d849552de414ba26400260cd
AO Industries, Inc. <support@aoindustries.com>
2016-10-09 22:51:56 -0500

    Set top comment correct for this submodule name.

--- src/main/java/com/aoindustries/messaging/tcp/client/TcpSocketClient.java
+++ src/main/java/com/aoindustries/messaging/tcp/client/TcpSocketClient.java
@@ -1,24 +1,24 @@
 /*
- * ao-messaging - Asynchronous bidirectional messaging over various protocols.
+ * ao-messaging-tcp-client - Client for asynchronous bidirectional messaging over TCP sockets.
  * Copyright (C) 2014, 2015, 2016  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
  *
- * This file is part of ao-messaging.
+ * This file is part of ao-messaging-tcp-client.
  *
- * ao-messaging is free software: you can redistribute it and/or modify
+ * ao-messaging-tcp-client is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ao-messaging is distributed in the hope that it will be useful,
+ * ao-messaging-tcp-client is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-messaging.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ao-messaging-tcp-client.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.aoindustries.messaging.tcp.client;
 
